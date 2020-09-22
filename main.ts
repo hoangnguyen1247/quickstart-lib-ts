6cd6d161905f3c6426e48ae64bcb24bec79b5aed
Hoàng Ng <hoangnguyen1247@gmail.com>
2020-09-22 15:34:55 +0700

    add docker

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 })
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     test();
     console.log("Server started in: " + PORT)
 })
